05e42e599117d7babc9ce0b19c9b41d75859911d
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2011-05-25 17:17:20 +0200

    Fixed analyze method

--- lib/mud/dependency.rb
+++ lib/mud/dependency.rb
@@ -2,14 +2,14 @@ module Mud
 
   class Dependency
     def self.analyze(src, context)
-      dependencies = []
+      dependencies = Set.new
 
       # Find all required files on the form require('name') og require('name', 'sub_name', ...)
       src.scan(/require\(((?:'[^']+'(?:,\s)?)+)\)/).each do |req|
-        reg.scan(/'([^']+)'/).each { |name| dependencies << new(name, context) }
+        req.first.scan(/'([^']+)'/).each { |name| dependencies << new(name.first, context) }
       end
 
-      dependencies
+      dependencies.to_a
     end
 
     attr_reader :name, :context
